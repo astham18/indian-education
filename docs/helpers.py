0af3e1ce64dc8fdbab2617614bb30f7c0fd8496e
Astha Mehta <astha.mehta.9@gmail.com>
2021-10-11 21:04:54 -0400

    Fix num_schools

--- docs/helpers.py
+++ docs/helpers.py
@@ -11,19 +11,29 @@ with open('docs/IND_adm2_Literacy.json', 'r+') as f:
     f.seek(0)
     json.dump(data, f, indent=4)
 
+from docs.data import NUM_SCHOOLS
 with open('docs/IND_adm2_Literacy.json', 'r+') as f:
     data = json.load(f)
     for y in range(len(data['objects']['IND_adm2']['geometries'])):
         data_obj = data['objects']['IND_adm2']['geometries'][y]
         data_id = data_obj['id'].lower()
+        data['objects']['IND_adm2']['geometries'][y]['properties']['Num_Schools'] = 0
         for i in range(len(NUM_SCHOOLS)):
             district = NUM_SCHOOLS[i]['district'].lower()
             if data_id == district:
                 data['objects']['IND_adm2']['geometries'][y]['properties']['Num_Schools'] = NUM_SCHOOLS[i]['num_schools']
-            if i == len(NUM_SCHOOLS) - 1:
-                data['objects']['IND_adm2']['geometries'][y]['properties']['Num_Schools'] = 0
     f.seek(0)
     json.dump(data, f, indent=4)
+
+for y in range(len(DATA['objects']['IND_adm2']['geometries'])):
+     data_obj = DATA['objects']['IND_adm2']['geometries'][y]
+     data_id = data_obj['id'].lower()
+     for i in range(len(NUM_SCHOOLS)):
+         district = NUM_SCHOOLS[i]['district'].lower()
+         if data_id == district:
+             DATA['objects']['IND_adm2']['geometries'][y]['properties']['Num_Schools'] = NUM_SCHOOLS[i]['num_schools']
+         if i == len(NUM_SCHOOLS) - 1:
+             DATA['objects']['IND_adm2']['geometries'][y]['properties']['Num_Schools'] = 0
 
 # To add new button,
 # Update json

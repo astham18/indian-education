3d0b59646f3563aa995b4193dc171810d34b9de3
Astha Mehta <astha.mehta.9@gmail.com>
2021-10-11 21:58:04 -0400

    test removing all

--- literacyMap.js
+++ literacyMap.js
@@ -41,8 +41,8 @@ function selectFilter() {
         selection.each(function () {
             // d3.select(this) creates a 1-element selection containing the current
             // node. This allows you to use D3's operators to modify the element
+            // "<input type='radio' name='gender' value='Literacy' checked> ALL<br>" +
             d3.select(this).html("<form>" +
-                "<input type='radio' name='gender' value='Literacy' checked> ALL<br>" +
                 "<input type='radio' name='gender' value='FemaleLiteracy'> FEMALE LITERACY<br>" +
                 "<input type='radio' name='gender' value='MaleLiteracy'> MALE LITERACY<br>" +
                 "<input type='radio' name='gender' value='Num_Schools'> Num_Schools<br>" +

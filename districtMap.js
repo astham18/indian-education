89c433ac93fc040e1cbdb953a29564ddbae1f2cd
Astha Mehta <astha.mehta.9@gmail.com>
2021-10-12 13:00:17 -0400

    Working version with separate districts

--- districtMap.js
+++ districtMap.js
@@ -1,122 +1,82 @@
-function districtMap(districts, disputed) {
+function districtMap(districts) {
 
-  var width = 800, height = 700, scale = 1200;
-  var propTag = 'Count', ttName = 'Count', unit = '';
+    var width = 500, height = 600, scale = 1200;
+    var propTag = 'Count', ttName = 'Count', unit = '';
 
-  function render(selection) {
-    selection.each(function () {
+    function render(selection) {
+        selection.each(function () {
 
-      d3.select(this).select("svg").remove();
-      var svg = d3.select(this).append("svg")
-        .attr("width", width)
-        .attr("height", height);
+            d3.select(this).select("svg").remove();
+            var svg = d3.select(this).append("svg")
+                .attr("width", width)
+                .attr("height", height);
 
-      d3.select(this).select("#tooltip").remove();
-      d3.select(this).append("div").attr("id", "tooltip").style("opacity", 0);
+            d3.select(this).select("#tooltip").remove();
+            d3.select(this).append("div").attr("id", "tooltip").style("opacity", 0);
 
-      var projection = d3.geo.mercator()
-        .center([83, 23])
-        .scale(scale)
-        .translate([width / 2, height / 2]);
+            var projection = d3.geoMercator()
+                .fitExtent([[7, 7], [width, height]], districts)
 
-      var path = d3.geo.path().projection(projection);
+            var path = d3.geoPath().projection(projection);
 
-      svg.selectAll(".district")
-        .data(districts.features)
-        .enter().append("path")
-        .attr("class", "district")
-        .style("fill", function (d) { return d.color; })
-        .attr("d", path)
-        .on("mouseover", function (d) {
-          d3.select("#tooltip").transition()
-            .duration(200)
-            .style("opacity", .9);
-          d3.select("#tooltip").html("<h3>" + (d.id) + "</h3><h4>(" + (d.properties.NAME_1) + ")</h4><table>" +
-            "<tr><td>" + ttName + "</td><td>" + (d.properties[propTag]) + unit + "</td></tr>" +
-            "</table>")
-            .style("left", (d3.event.pageX - document.getElementById('map').offsetLeft + 20) + "px")
-            .style("top", (d3.event.pageY - document.getElementById('map').offsetTop - 60) + "px");
-        })
-        .on("mouseout", function (d) {
-          d3.select("#tooltip").transition()
-            .duration(500)
-            .style("opacity", 0);
-        })
-        .on("click", clicked);
 
-      svg.selectAll(".disputed")
-        .data(disputed.features)
-        .enter().append("path")
-        .attr("class", "disputed")
-        .style("fill", function (d) { return d.color; })
-        .attr("d", path);
-      
-      // New code
-      var active = d3.select(null)
+            svg.selectAll(".district")
+                .data(districts.features)
+                .enter().append("path")
+                .attr("class", "district")
+                .style("fill", function (d) { return d.color; })
+                .attr("d", path)
+                .on("mouseover", function (d) {
+                    d3.select("#tooltip").transition()
+                        .duration(200)
+                        .style("opacity", .9);
+                    d3.select("#tooltip").html("<h3>" + (d.id) + "</h3><h4>(" + (d.properties.NAME_1) + ")</h4><table>" +
+                        "<tr><td>" + ttName + "</td><td>" + (d.properties[propTag]) + unit + "</td></tr>" +
+                        "</table>")
+                        .style("left", (d3.event.pageX - document.getElementById('map').offsetLeft + 20) + "px")
+                        .style("top", (d3.event.pageY - document.getElementById('map').offsetTop - 60) + "px");
+                })
+                .on("mouseout", function (d) {
+                    d3.select("#tooltip").transition()
+                        .duration(500)
+                        .style("opacity", 0);
+                })
+              
 
-      function clicked(d) {
 
-        if (active.node() === this) return reset();
-        active.classed("active", false);
-        active = d3.select(this).classed("active", true);
+            
+        });
+    } // render
+    render.height = function (value) {
+        if (!arguments.length) return height;
+        height = value;
+        return render;
+    };
+    render.width = function (value) {
+        if (!arguments.length) return width;
+        width = value;
+        return render;
+    };
+    render.scale = function (value) {
+        if (!arguments.length) return scale;
+        scale = value;
+        return render;
+    };
+    render.propTag = function (value) {
+        if (!arguments.length) return propTag;
+        propTag = value;
+        return render;
+    };
+    render.ttName = function (value) {
+        if (!arguments.length) return ttName;
+        ttName = value;
+        return render;
+    };
+    render.unit = function (value) {
+        if (!arguments.length) return unit;
+        unit = value;
+        return render;
+    };
 
-        var bounds = path.bounds(d),
-          dx = bounds[1][0] - bounds[0][0],
-          dy = bounds[1][1] - bounds[0][1],
-          x = (bounds[0][0] + bounds[1][0]) / 2,
-          y = (bounds[0][1] + bounds[1][1]) / 2,
-          scale = .9 / Math.max(dx / width, dy / height),
-          translate = [width / 2 - scale * x, height / 2 - scale * y];
-
-        d3.select("#tooltip").transition()
-          .duration(750)
-          .style("stroke-width", 1.5 / scale + "px")
-          .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
-      console.log("clicked")
-      }
-
-      function reset() {
-        active.classed("active", false);
-        active = d3.select(null);
-
-        d3.select("#tooltip").transition()
-          .duration(750)
-          .style("stroke-width", "1.5px")
-          .attr("transform", "");
-      }
-    // end new code
-    });
-  } // render
-  render.height = function (value) {
-    if (!arguments.length) return height;
-    height = value;
     return render;
-  };
-  render.width = function (value) {
-    if (!arguments.length) return width;
-    width = value;
-    return render;
-  };
-  render.scale = function (value) {
-    if (!arguments.length) return scale;
-    scale = value;
-    return render;
-  };
-  render.propTag = function (value) {
-    if (!arguments.length) return propTag;
-    propTag = value;
-    return render;
-  };
-  render.ttName = function (value) {
-    if (!arguments.length) return ttName;
-    ttName = value;
-    return render;
-  };
-  render.unit = function (value) {
-    if (!arguments.length) return unit;
-    unit = value;
-    return render;
-  };
-
-  return render;
 } // districtMap
--- literacyMap.js
+++ literacyMap.js
@@ -1,14 +1,66 @@
 (function () {
     d3.queue()
         .defer(d3.json, "IND_adm2_Literacy.json")
-        .defer(d3.json, "ne_10m_admin_0_Kashmir_Occupied.json")
-        .await(function (error, topoMain, topoKashmir) {
+        .await(function (error, topoMain) {
             var districts, disputed;
             if (error) throw error;
 
             // Features for districts and disputed areas
             districts = topojson.feature(topoMain, topoMain.objects.IND_adm2);
-            disputed = topojson.feature(topoKashmir, topoKashmir.objects.ne_10m_admin_0_Kashmir_Occupied);
+
+            // Select List
+            let stateList = districts.features.map(d => d.properties.NAME_1);
+            stateList = [...new Set(stateList)];
+            let stateOptions = `<option selected value='All State'>All States</option>`;
+            stateList.forEach(d => {
+                stateOptions = stateOptions + `<option>${d}</option>`;
+            });
+
+            d3.select('#stateList').html(stateOptions);
+            var STATE_VAL = 'All States'
+            document.getElementById('stateList').addEventListener('change', function () {
+                //console.log('You selected: ', this.value);
+                STATE_VAL = this.value
+                console.log(STATE_VAL)
+                //
+                var width = 500,
+                    height = 600;
+                // Filter data
+
+                let preparedData = districts.features.filter(d => d.properties.NAME_1 == STATE_VAL);
+                if (STATE_VAL == "All State") {
+                    preparedData = districts.features
+                    width = 800;
+                    height = 700;
+                }
+                preparedData = {
+                    type: "FeatureCollection",
+                    features: preparedData
+                }
+                console.log(preparedData)
+                d3.select("#select").call(selectFilter());
+                var filter = d3.select('#select input[name="gender"]:checked').node().value;
+
+                colorCode(preparedData.features, filter);
+                
+                var map = districtMap(preparedData).width(width).height(height).scale(1200).propTag(filter);
+                d3.select("#map").call(map);
+
+                // On change of selection re-render
+                d3.selectAll("#select input[name=gender]").on("change", function () {
+                    filter = d3.select('#select input[name="gender"]:checked').node().value;
+                    colorCode(preparedData.features, filter);
+                    map = districtMap(preparedData).width(width).height(height).scale(1200).propTag(filter);
+                    d3.select("#map").call(map);
+                });
+
+            });
+
+
+            // console.log(d3.select("#stateList").node().value)
+
+            //let preparedData = districts.features.filter(d => d.properties.NAME_1 == d3.select("#stateList").node().value);
+            //console.log(preparedData)
 
             // Radio HTML
             // selectFilter is the function that generates the radio buttons
@@ -20,17 +72,16 @@
 
             // Color codes for districts based on Literacy Rates
             colorCode(districts.features, filter);
-            colorDisputed(disputed.features);
 
             // Map render
-            var map = districtMap(districts, disputed).width(800).height(700).scale(1200).propTag(filter);
+            var map = districtMap(districts).width(800).height(700).scale(1200).propTag(filter);
             d3.select("#map").call(map);
 
             // On change of selection re-render
             d3.selectAll("#select input[name=gender]").on("change", function () {
                 filter = d3.select('#select input[name="gender"]:checked').node().value;
                 colorCode(districts.features, filter);
-                map = districtMap(districts, disputed).width(800).height(700).scale(1200).propTag(filter);
+                map = districtMap(districts).width(800).height(700).scale(1200).propTag(filter);
                 d3.select("#map").call(map);
             });
         });
@@ -50,37 +101,24 @@ function selectFilter() {
         });
     } // render
     return render;
-} // selectFilter
-
-function colorCode(data, filter) {
-    /*
-    var color = d3.scale.quantize()
-        .domain(d3.extent(data))
-        .range(["#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3"]);*/
-    var keyArray = data.map(function (item) { return item["properties"][filter]; });
-    console.log(keyArray)
-    var sorted_data = keyArray.sort(d3.ascending);
-    var quantile_025 = d3.quantile(sorted_data, 0.25)
-    console.log(quantile_025)
-    var quantile_050 = d3.quantile(sorted_data, 0.50)
-    console.log(quantile_050)
-    var quantile_075 = d3.quantile(sorted_data, 0.75)
-    console.log(quantile_075)
-
-    var color = d3.scale.threshold()
-        // .domain([65, 72, 78, 85])
-        .domain([quantile_025, quantile_050, quantile_075])
-        //.range(["#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3"]);
-        .range(["#dadaeb", "#9e9ac8", "#807dba", "#6a51a3"]);
-    data.forEach(function (d) {
-        if (isNaN(d.properties[filter])) { d.properties[filter] = 77; }
-        d.color = color(d.properties[filter]);
-    });
 }
 
-function colorDisputed(data) {
-    var color = "#eaeafa";
+function colorCode(data, filter) {
+
+    var keyArray = data.map(function (item) { return item["properties"][filter]; });
+    var sorted_data = keyArray.sort(d3.ascending);
+    var quantile_025 = d3.quantile(sorted_data, 0.25)
+    var quantile_050 = d3.quantile(sorted_data, 0.50)
+    var quantile_075 = d3.quantile(sorted_data, 0.75)
+
+    // colors ["#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3"]);
+    var color = d3.scaleThreshold()
+        .domain([quantile_025, quantile_050, quantile_075])
+        .range(["#dadaeb", "#9e9ac8", "#807dba", "#6a51a3"]);
+        
     data.forEach(function (d) {
-        d.color = color;
+        if (isNaN(d.properties[filter])) { d.properties[filter] = 0; }
+        if (d.properties[filter] == 0) { d.color = "white"; }
+        else { d.color = color(d.properties[filter]);}
     });
 }
